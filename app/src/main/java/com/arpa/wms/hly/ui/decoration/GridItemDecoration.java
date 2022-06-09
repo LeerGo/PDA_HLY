dcfb24eb5629eece05f3a22309bedfd4ea9813b7
李一方 <leergo@dingtalk.com>
2022-06-09 16:34:01 +0800

    fix: ui 显示错误
    Signed-off-by: 李一方 <leergo@dingtalk.com>

--- app/src/main/java/com/arpa/wms/hly/ui/decoration/GridItemDecoration.java
+++ app/src/main/java/com/arpa/wms/hly/ui/decoration/GridItemDecoration.java
@@ -2,10 +2,12 @@ package com.arpa.wms.hly.ui.decoration;
 
 import android.graphics.Rect;
 import android.view.View;
-
 import com.arpa.wms.hly.utils.DensityUtils;
 
+import java.util.Objects;
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
@@ -23,16 +25,20 @@ public class GridItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
-        outRect.top = interval;
-        if (parent.getChildAdapterPosition(view) % 2 == 0) {
+        GridLayoutManager manager = (GridLayoutManager) parent.getLayoutManager();
+        int spanCount = Objects.requireNonNull(manager).getSpanCount();
+        int position = parent.getChildAdapterPosition(view);
+
+        if (position < spanCount) {
+            outRect.top = interval;
+        }
+        outRect.bottom = interval;
+        if (position % spanCount == 0) {
             outRect.left = interval;
             outRect.right = interval / 2;
-        } else if (parent.getChildAdapterPosition(view) % 2 == 1) {
+        } else if (position % spanCount == 1) {
             outRect.left = interval / 2;
             outRect.right = interval;
-        }
-        if (parent.getChildAdapterPosition(view) == state.getItemCount() - 1) {
-            outRect.bottom = interval;
         }
     }
 }
