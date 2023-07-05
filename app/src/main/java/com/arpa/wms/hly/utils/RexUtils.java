eb24aa9796d301df6d1341a6a8b44ff0bfe1ad58
李一方 <leergo@dingtalk.com>
2023-07-05 09:03:05 +0800

    ver: 1.5.9
    Signed-off-by: 李一方 <leergo@dingtalk.com>

--- app/src/main/java/com/arpa/wms/hly/utils/RexUtils.java
+++ app/src/main/java/com/arpa/wms/hly/utils/RexUtils.java
@@ -23,7 +23,7 @@ public class RexUtils {
     private static final String REX_24_HOUR = "(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d";
     // private static final String REX_YYYYMMDD= "((\\d{3}[1-9]|\\d{2}[1-9]\\d|\\d[1-9]\\d{2}|[1-9]\\d{3})(((0[13578]|1[02])(0[1-9]|[12]\\d|3[01]))|((0[469]|11)(0[1-9]|[12]\\d|30))|(02(0[1-9]|[1]\\d|2[0-8]))))|(((\\d{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))0229)";
     private static final String REX_YYYYMMDD = "^\\d{4}-\\d{2}-\\d{2}$";
-    private static final String REX_ADDRESS = "[A-Z]|[A-Z]\\d{1,2}";
+    private static final String REX_ADDRESS = "[A-Z]{1,2}|[A-Z]\\d{1,2}";
     private static final List<String> ADDRESS_SPECIAL = Arrays.asList("M1", "M2", "M3");
 
     public static boolean isBatchNo(String data) {
